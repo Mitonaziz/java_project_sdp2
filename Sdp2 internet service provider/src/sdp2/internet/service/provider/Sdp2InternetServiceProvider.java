be87a1275980dfc075b459f5c8792cc5302cd2c7
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2022-10-23 23:22:19 +0600

    print nasim
    dfjdsf

--- Sdp2 internet service provider/src/sdp2/internet/service/provider/Sdp2InternetServiceProvider.java
+++ Sdp2 internet service provider/src/sdp2/internet/service/provider/Sdp2InternetServiceProvider.java
@@ -10,11 +10,13 @@ package sdp2.internet.service.provider;
  */
 public class Sdp2InternetServiceProvider {
 
-    /**
+    /**hid sdffdksfjdjf  dskfjlkdsfjdkf
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Nasim");
     }
     
 }
