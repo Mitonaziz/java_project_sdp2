eed4ab7178197a2c97f8404a5e6f536c18e696de
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2022-10-24 00:27:29 +0600

    new

--- Sdp2 internet service provider/src/Home/Home.java
+++ Sdp2 internet service provider/src/Home/Home.java
@@ -8,12 +8,12 @@ package Home;
  *
  * @author mdazizurrahman
  */
-public class Home_Form_1 extends javax.swing.JFrame {
+public class Home extends javax.swing.JFrame {
 
     /**
-     * Creates new form Home_Form_1
+     * Creates new form Home
      */
-    public Home_Form_1() {
+    public Home() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Home_Form_1 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Home_Form_1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Home_Form_1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Home_Form_1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Home_Form_1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Home_Form_1().setVisible(true);
+                new Home().setVisible(true);
             }
         });
     }
